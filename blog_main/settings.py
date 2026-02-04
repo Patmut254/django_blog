59dd882903170715adba274abd7118dd10c4ee8c
Patmut254 <anitajehu13@gmail.com>
2026-02-04 23:21:36 +0300

    write the home page code and load static

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -115,3 +115,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_DIRS = [
+    'blog_main/static'
+    BASE_DIR / 'static'
+]
+
+
