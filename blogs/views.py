5cddc4a4f453b79a895a174d84ff05f95d375646
Patmut254 <anitajehu13@gmail.com>
2026-02-05 23:12:10 +0300

    add get object or 404 to deal with category error

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Blog, Category
+
 
 # Create your views here.
 
@@ -8,7 +9,12 @@ def posts_by_category(request, category_id):
     # fetch the posts taht belongs to the category with the id category_id
     # foreign key will always store the id
     posts = Blog.objects.filter(status='Published', category_id=category_id)
-    category = Category.objects.get(pk=category_id)
+    # try:
+    #     category = Category.objects.get(pk=category_id)
+    # except:
+    #     pass
+    category =get_object_or_404(Category, pk=category_id)
+    
     context = {
         'posts' : posts,
         'category' : category,
