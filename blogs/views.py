5b77e181bcc37532e274f74ccd2636642017e68f
Patmut254 <anitajehu13@gmail.com>
2026-02-05 23:06:45 +0300

    add a posts by category html

--- blogs/views.py
+++ blogs/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Blog
+from .models import Blog, Category
 
 # Create your views here.
 
@@ -8,4 +8,9 @@ def posts_by_category(request, category_id):
     # fetch the posts taht belongs to the category with the id category_id
     # foreign key will always store the id
     posts = Blog.objects.filter(status='Published', category_id=category_id)
-    return HttpResponse(posts)
+    category = Category.objects.get(pk=category_id)
+    context = {
+        'posts' : posts,
+        'category' : category,
+    }
+    return render(request, 'posts_by_category.html', context)
