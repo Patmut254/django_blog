13f01926aec18991a3b522fa74ad3c05fa1bfded
Patmut254 <anitajehu13@gmail.com>
2026-02-05 14:59:09 +0300

    sync the categories loaded in the backend admin to the home template

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,13 @@
 
 from django.shortcuts import render
+from blogs.models import Blog, Category
+
+# Create your views here.
 
 
 def home(request):
-    return render(request, 'home.html')
+    categories = Category.objects.all()
+    context = {
+        'categories': categories
+    }
+    return render(request, 'home.html', context)
