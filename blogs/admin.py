846294d014c852eff34d67d40cf1e85576388479
Patmut254 <anitajehu13@gmail.com>
2026-02-05 14:14:01 +0300

    implement the created at and search feature for admin

--- blogs/admin.py
+++ blogs/admin.py
@@ -4,6 +4,9 @@ from .models import Category, Blog
 
 class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
+    list_display = ('title', 'category', 'author', 'status', 'is_featured', 'created_at', 'updated_at')
+    search_fields = ('id', 'title', 'category__category_name', 'status')
+
 
 
 admin.site.register(Category)
