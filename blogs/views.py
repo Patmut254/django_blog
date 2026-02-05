6efcbfeb8695bb2cd7d179b338b82d9d8f936193
Patmut254 <anitajehu13@gmail.com>
2026-02-05 22:34:23 +0300

    add a category fetching property and url path

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Blog
 
 # Create your views here.
+
+def posts_by_category(request, category_id):
+    # fetch the posts taht belongs to the category with the id category_id
+    # foreign key will always store the id
+    posts = Blog.objects.filter(status='Published', category_id=category_id)
+    return HttpResponse(posts)
