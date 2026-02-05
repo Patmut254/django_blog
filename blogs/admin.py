9aef9420926ece27e565d60de4fb9b4878a284e4
Patmut254 <anitajehu13@gmail.com>
2026-02-05 14:18:02 +0300

    make status editable

--- blogs/admin.py
+++ blogs/admin.py
@@ -6,6 +6,8 @@ class BlogAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     list_display = ('title', 'category', 'author', 'status', 'is_featured', 'created_at', 'updated_at')
     search_fields = ('id', 'title', 'category__category_name', 'status')
+    list_editable = ('is_featured', )
+
 
 
 
