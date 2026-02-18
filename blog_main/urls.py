2f5c654ff632177821a933843df12b3a308899a7
Patmut254 <anitajehu13@gmail.com>
2026-02-18 23:38:11 +0300

    add logout url, its function and import to the base

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -35,6 +35,8 @@ urlpatterns = [
     path('register/', views.register, name='register'),
 
     path('login/', views.login, name='login'),
+
+    path('logout/', views.logout, name='logout'),
 ]
 
 if settings.DEBUG:
