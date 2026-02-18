83a8ca2cc7f044c29b6050d41872230c9ab65813
Patmut254 <anitajehu13@gmail.com>
2026-02-18 23:27:33 +0300

    add user validation and redirect to home page if user exists

--- blog_main/views.py
+++ blog_main/views.py
@@ -4,6 +4,7 @@ from blogs.models import Blog, Category
 from assignments.models import About
 from .forms import RegistrationForm
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
 
 # Create your views here.
 
@@ -44,6 +45,16 @@ def register(request):
     return render(request, 'register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+            return redirect('home')
     form = AuthenticationForm()
     context = {
         'form': form,
