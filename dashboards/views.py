d3b309904b924fc76819744f950122fde540652d
Patmut254 <anitajehu13@gmail.com>
2026-02-21 20:08:02 +0300

    add login_required utility to restrict dashboard view

--- dashboards/views.py
+++ dashboards/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render
 
 from blogs.models import Category, Blog
 
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='login')
 # Create your views here.
 def dashboard(request):
     category_count = Category.objects.all().count()
