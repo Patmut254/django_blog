3b07defa363298674f6ed58848bc2d397267b770
Patmut254 <anitajehu13@gmail.com>
2026-02-21 23:14:47 +0300

    add side par urls

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.dashboard, name='dashboard'),
+    path('categories/', views.categories, name='categories'),
+    path('blogs/', views.categories, name='blogs'),
+    path('logout/', views.categories, name='logout'),
 ]
