35c78b47942578c08ce277d17dc3e351eb32e361
Patmut254 <anitajehu13@gmail.com>
2026-02-21 20:01:20 +0300

    add dashboard function to handle blog and category counts

--- dashboards/views.py
+++ dashboards/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
+from blogs.models import Category, Blog
+
+
 # Create your views here.
-def dashboard
+def dashboard(request):
+    category_count = Category.objects.all().count()
+    blogs_count = Blog.objects.all().count()
+
+
+    context ={
+        'category_count':category_count,
+        'blogs_count': blogs_count,
+    }
+    return render(request, 'dashboard/dashboard.html', context)
+
