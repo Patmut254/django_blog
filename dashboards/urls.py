ffa6d13e865a32dc39e852a1a4ba165dab821a4a
Patmut254 <anitajehu13@gmail.com>
2026-02-24 23:17:20 +0300

    posts/blogs url utilization

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('categories/', views.categories, name='categories'),
     path('blogs/', views.categories, name='blogs'),
     path('logout/', views.categories, name='logout'),
+    # caterogy crud
     path('categories/add/', views.add_category, name='add_category'),
     path('categories/edit/<int:pk>/', views.edit_category, name='edit_category'),
     path('categories/delete/<int:pk>/', views.delete_category, name='delete_category'),
+    # blog post crud
+    path('posts/', views.posts, name='posts'),
 ]
